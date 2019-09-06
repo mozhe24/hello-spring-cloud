5db472722767060984e0b1fb07ee1f63456e855b
caodongj <1037444979@qq.com>
2019-09-06 16:20:43 +0800

    添加spring-cloud-bus

--- hello-spring-cloud-service-admin/src/main/java/com/funtl/hello/spring/cloud/service/admin/ServiceAdminApplication.java
+++ hello-spring-cloud-service-admin/src/main/java/com/funtl/hello/spring/cloud/service/admin/ServiceAdminApplication.java
@@ -1,8 +1,13 @@
 package com.funtl.hello.spring.cloud.service.admin;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @Author
@@ -13,9 +18,20 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  */
 @EnableEurekaClient
 @SpringBootApplication
+@EnableDiscoveryClient
+@RefreshScope
+@RestController
 public class ServiceAdminApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ServiceAdminApplication.class, args);
+    }
+
+    @Value("${spring.application.name}")
+    String name;
+
+    @RequestMapping(value = "/hello")
+    public String hello(){
+        return name;
     }
 }
